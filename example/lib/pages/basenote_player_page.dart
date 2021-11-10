cb9bd6fd508334b781638beeb9a3b6c5c1cffe32
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2021-11-10 15:05:35 -0700

    wip

---
+++ example/lib/pages/basenote_player_page.dart
@@ -0,0 +1,99 @@
+import 'package:better_player/better_player.dart';
+import 'package:better_player_example/constants.dart';
+import 'package:flutter/material.dart';
+
+class BasenotePlayerPage extends StatefulWidget {
+  @override
+  _BasenotePlayerPageState createState() => _BasenotePlayerPageState();
+}
+
+class _BasenotePlayerPageState extends State<BasenotePlayerPage> {
+  late BetterPlayerController _betterPlayerController;
+  late BetterPlayerDataSource _betterPlayerDataSource;
+
+  @override
+  void initState() {
+    BetterPlayerConfiguration betterPlayerConfiguration =
+        BetterPlayerConfiguration(
+      aspectRatio: 16 / 9,
+      fit: BoxFit.contain,
+    );
+    _betterPlayerDataSource = BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      "https://videodelivery.net/9dd71b21e6694c84bb5f49e906668d01/manifest/video.m3u8",
+      cacheConfiguration: BetterPlayerCacheConfiguration(
+        useCache: true,
+        preCacheSize: 10 * 1024 * 1024,
+        maxCacheSize: 10 * 1024 * 1024,
+        maxCacheFileSize: 10 * 1024 * 1024,
+
+        ///Android only option to use cached video between app sessions
+        key: "testCacheKey",
+      ),
+      notificationConfiguration: BetterPlayerNotificationConfiguration(
+        showNotification: true,
+        title: "Elephant dream",
+        author: "Some author",
+        imageUrl: Constants.catImageUrl,
+      ),
+    );
+    _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
+
+    _betterPlayerController.addEventsListener((p0) {
+      print(p0.betterPlayerEventType);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cache"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Player with cache enabled. To test this feature, first plays "
+              "video, then leave this page, turn internet off and enter "
+              "page again. You should be able to play video without "
+              "internet connection.",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: BetterPlayer(controller: _betterPlayerController),
+          ),
+          TextButton(
+            child: Text("Start pre cache"),
+            onPressed: () {
+              _betterPlayerController.preCache(_betterPlayerDataSource);
+            },
+          ),
+          TextButton(
+            child: Text("Stop pre cache"),
+            onPressed: () {
+              _betterPlayerController.stopPreCache(_betterPlayerDataSource);
+            },
+          ),
+          TextButton(
+            child: Text("Play video"),
+            onPressed: () {
+              _betterPlayerController.setupDataSource(_betterPlayerDataSource);
+            },
+          ),
+          TextButton(
+            child: Text("Clear cache"),
+            onPressed: () {
+              _betterPlayerController.clearCache();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

a45ae44a661ae4c00574e9ffac82ccb786768072
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2022-06-24 10:51:11 -0400

    Upgrade dependencies.

--- lib/src/core/better_player.dart
+++ lib/src/core/better_player.dart
@@ -66,7 +66,7 @@ class _BetterPlayerState extends State<BetterPlayer>
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addObserver(this);
+    WidgetsBinding.instance.addObserver(this);
   }
 
   @override
@@ -113,7 +113,7 @@ class _BetterPlayerState extends State<BetterPlayer>
           _betterPlayerConfiguration.deviceOrientationsAfterFullScreen);
     }
 
-    WidgetsBinding.instance!.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     _controllerEventSubscription?.cancel();
     widget.controller.dispose();
     VisibilityDetectorController.instance
